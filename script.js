855ce746e92ba07d099f5d548ef1af4e611909c2
Phelipe SR <phelipeuni@gmail.com>
2021-12-26 13:39:29 -0300

    resolvendo bug timezone

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   $('.countdown').countdown({
     date: '05/14/2022 15:35:00',
+    offset: -3,
     day: 'Dia',
     days: 'Dias',
     hour: 'Hora',
